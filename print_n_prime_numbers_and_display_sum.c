ea9cd760c477b10032beb5445e9133abf64e55eb
Sakar Aryal <67322716+techiesakar@users.noreply.github.com>
2021-12-07 15:45:16 +0545

    Minor taxo fix

--- print_n_prime_numbers_and_display_sum.c
+++ print_n_prime_numbers_and_display_sum.c
@@ -9,7 +9,7 @@
 #include <math.h>
 int main() {
     int i, prime = 2, is_prime = 2, count = 0, term, sum = 0, flag = 0;
-    printf("\nEnter the total even prime number you wanted : ");
+    printf("\nEnter the total prime number you wanted : ");
     scanf("%d", & term);
     while (count != term) {
         if (count == 0 || count == 1) {
